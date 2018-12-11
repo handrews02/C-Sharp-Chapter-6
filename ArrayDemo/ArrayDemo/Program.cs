17c6a7667fc25575849fc020ef4d559fcdd5952a
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-11 13:04:34 -0600

    update text

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] entries;
+            entries = new int[8];
+            int i = 0;
+            int location;
+            int guess = 0;
+
+
+            for(i = 0; i < 8; i++) {
+                Write("Enter a number " + i + " >> ");
+                entries[i] = Convert.ToInt32(ReadLine());   
+            }
+            while(guess != 999)
+            {
+                WriteLine("enter '1' to see them in order. enter '2' to see them in reverse order. enter '3' to display a specific location.");
+                guess = Convert.ToInt32(ReadLine());
+
+                if (guess == 1)
+                {
+
+                    for (int j = 0; j < entries.Length; j++)
+                    {
+                        WriteLine("Numbers entered in order were " + entries[j]);
+                    }
+                }
+                else if (guess == 2)
+                {
+
+                    Array.Reverse(entries);
+                    for (int z = 0; z < entries.Length; z++)
+                    {
+                        WriteLine("Numbers in reverser order were " + entries[z]);
+                    }
+                }
+                else if(guess == 3)
+                {
+                    WriteLine("Enter a location >> ");
+                    location = Convert.ToInt32(ReadLine());
+
+                    WriteLine(entries[location - 1]);
+                }
+            }
+       
+
+        }
+    }
+}
---
+++ SavingIntoAnArray/SavingIntoAnArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace SavingIntoAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i;
+            Write("\n\nRead and Print elements of an array:\n");
+            Write("-----------------------------------------\n");
+            Write("Input 10 elements in the array");
+            for(i=0; i < 10; i++)
+            {
+                Write("Enter number - " + i + " << ");
+                arr[i] = Convert.ToInt32(ReadLine());
+            }
+            for(int j = 0; j <arr.Length; j++)
+            {
+                WriteLine("Numbers entered were " + arr[j]);
+            }
+
+        }
+    }
+}
