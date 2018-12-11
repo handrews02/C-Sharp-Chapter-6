0603521f6d0055e3ae0625dc1058469083e52c3c
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-11 13:53:51 -0600

    Update Program.cs

--- ScoresComparison/ScoresComparison/Program.cs
+++ ScoresComparison/ScoresComparison/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ScoresComparison
 {
@@ -10,6 +11,25 @@ namespace ScoresComparison
     {
         static void Main(string[] args)
         {
+            int[] tests = new int[4];
+            int previousScore = 0;
+            for(int i = 0; i < 4; i++)
+            {
+                Write("Enter 4 test scores >> ");
+                tests[i] = Convert.ToInt32(ReadLine());
+            }
+            for (int j = 0; j < tests.Length; j++)
+            {
+                if (tests[j]>previousScore) {
+                    previousScore = tests[j];//high score
+                    WriteLine("Congrats, this is a new high score!");
+                }
+                else if (tests[j] < previousScore)
+                {
+                    WriteLine("Unfourtantley you didn't get higher than the highest score!");
+                }
+                
+            }
         }
     }
 }
