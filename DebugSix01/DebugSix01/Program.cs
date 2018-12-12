e5f0fd9755de1ff417dee388d75a4f40cbad159e
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-12 14:23:24 -0600

    update

---
+++ DebugSix01/DebugSix01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugSix01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = { "Belt", "Tie", "Scarf", "Cufflinks" };
+            double[] prices = { 29.00, 35.95, 18.50, 112.99 };
+            WriteLine("Items for sale:");
+            for (int x = 0; x <= 4; ++x)
+                WriteLine("{0, 10} for {1, 10}", items[x], prices[x].ToString("C"));
+        }
+    }
+}
---
+++ DebugSix02/DebugSix02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugSix02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 15, 22, 88 };
+            int x;
+            double average;
+            double total = 0;
+            Write("\nThe numbers are...");
+            for (x = 0; x <= numbers.Length; ++x)
+                Write("{0, 6}", numbers[x]);
+               WriteLine();
+            for (x = 0; x < numbers.Length; ++x)
+            {
+                total = numbers[x];
+            }
+            average = total / numbers.Length;
+            Write("The average is {0}");
+        }
+    }
+}
